0c5d7efc64a4558e917a8b8d11a2dbe9f008fb17
fmartos <fmartos>
2005-02-28 21:34:56 +0000

    Primera prueba de subida usando CVS

---
+++ Sigrem/main/ModuloGestion.java
@@ -0,0 +1,17 @@
+/*
+ * Created on 28-feb-2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package main;
+
+/**
+ * @author felixmt
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public interface ModuloGestion {
+
+}
