86360f0ccc3f9d1fe2698b6a144dc9636ff92fdb
javi_is <javi_is>
2005-03-01 01:36:55 +0000

    *** empty log message ***

---
+++ InterfazGrafica/ClasePrincipal.java
@@ -0,0 +1,12 @@
+import interfaz.*;
+
+public class ClasePrincipal 
+{
+	private static InterfazGrafica ig;
+	
+	public static void main(String[] args) 
+	{
+		InterfazGrafica ig=new InterfazGrafica();
+		ig.activa();
+	}
+}
