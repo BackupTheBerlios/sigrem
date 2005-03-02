0193dd6f7517ca8077f49cce412f31001c371445
miguelr80 <miguelr80>
2005-03-02 02:39:44 +0000

    Alguna cosilla más...

--- InterfazGrafica/interfaz/PanelEmpleados.java
+++ InterfazGrafica/interfaz/PanelEmpleados.java
@@ -14,13 +14,16 @@ import java.awt.*;
 
 public class PanelEmpleados extends JPanel 
 {
-	private JFrame nemp; 
+	public JFrame formulario; 
 	
 	public PanelEmpleados()
 	{
 		super();
+		formulario=new JFrame();
+		formulario.setResizable(false);
+		formulario.setLocation(350,100);
 		JPanel pempleado=dibujaEmpleado();
-		JPanel pdatos=dibujaDatos();
+		JPanel pdatos=dibujaDatos(false);
 		JScrollPane precursos=dibujaRecursos();
 		JSplitPane sp1=new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,pempleado,pdatos);
 		sp1.setDividerSize(4);
@@ -102,7 +105,7 @@ public class PanelEmpleados extends JPanel
 		return pemp;
 	}
 	
-	public JPanel dibujaDatos() 
+	public JPanel dibujaDatos(boolean editable) 
 	{		
 		JPanel pdat=new JPanel();
 		pdat.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),"Datos personales",TitledBorder.LEFT,TitledBorder.TOP));
@@ -148,22 +151,25 @@ public class PanelEmpleados extends JPanel
 		tpro.setPreferredSize(new Dimension(160,20));
 		ttel.setPreferredSize(new Dimension(80,20));
 		temail.setPreferredSize(new Dimension(80,20));
-		tnom.setEditable(false);
-		tdni.setEditable(false);
-		tdir.setEditable(false);
-		tcp.setEditable(false);
-		tloc.setEditable(false);
-		tpro.setEditable(false);
-		ttel.setEditable(false);
-		temail.setEditable(false);
-		tnom.setBackground(Color.YELLOW);
-		tdni.setBackground(Color.YELLOW);
-		tdir.setBackground(Color.YELLOW);
-		tcp.setBackground(Color.YELLOW);
-		tloc.setBackground(Color.YELLOW);
-		tpro.setBackground(Color.YELLOW);
-		ttel.setBackground(Color.YELLOW);
-		temail.setBackground(Color.YELLOW);
+		if (!editable)
+		{
+			tnom.setEditable(false);
+			tdni.setEditable(false);
+			tdir.setEditable(false);
+			tcp.setEditable(false);
+			tloc.setEditable(false);
+			tpro.setEditable(false);
+			ttel.setEditable(false);
+			temail.setEditable(false);
+			tnom.setBackground(Color.YELLOW);
+			tdni.setBackground(Color.YELLOW);
+			tdir.setBackground(Color.YELLOW);
+			tcp.setBackground(Color.YELLOW);
+			tloc.setBackground(Color.YELLOW);
+			tpro.setBackground(Color.YELLOW);
+			ttel.setBackground(Color.YELLOW);
+			temail.setBackground(Color.YELLOW);
+		}
 		Box c1=Box.createHorizontalBox();
 		Box c2=Box.createHorizontalBox();
 		Box c3=Box.createHorizontalBox();
@@ -220,110 +226,116 @@ public class PanelEmpleados extends JPanel
 	}
 	
 	public void creaEmpleado()
-	{
-		nemp=new JFrame("Nuevo Empleado");
-		JPanel pd=dibujaNuevosDatos();
-		nemp.getContentPane().add(pd);
-		nemp.pack();
-		nemp.setLocation(100,50);
-		nemp.setResizable(false);
-		nemp.setVisible(true);
-		nemp.addWindowListener(new WindowAdapter()
+	{		
+		JPanel pd=panelAltaEmpleado();
+		formulario.getContentPane().add(pd);
+		formulario.pack();
+		formulario.setVisible(true);
+		formulario.addWindowListener(new WindowAdapter()
 		{	public void windowClosing(WindowEvent e)
 			{
-				nemp.setVisible(false);	
+				formulario.setVisible(false);	
 			}
 		});	
 	}
 	
-	public JPanel dibujaNuevosDatos() 
+	public JPanel panelAltaEmpleado() 
 	{		
-		JPanel pndat=new JPanel();
-		pndat.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),"Datos personales",TitledBorder.LEFT,TitledBorder.TOP));
-		JLabel l1=new JLabel("Nombre ",SwingConstants.RIGHT);
-		JLabel l2=new JLabel("DNI/CIF ",SwingConstants.RIGHT);
-		JLabel l3=new JLabel("Dirección ",SwingConstants.RIGHT);
-		JLabel l4=new JLabel("Código postal ",SwingConstants.RIGHT);
-		JLabel l5=new JLabel("Localidad ",SwingConstants.RIGHT);
-		JLabel l6=new JLabel("Provincia ",SwingConstants.RIGHT);		
-		JLabel l7=new JLabel("Teléfono ",SwingConstants.RIGHT);
-		JLabel l8=new JLabel("email ",SwingConstants.RIGHT);
-		l1.setPreferredSize(new Dimension(80,20));
-		l2.setPreferredSize(new Dimension(80,20));
-		l3.setPreferredSize(new Dimension(80,20));
-		l4.setPreferredSize(new Dimension(80,20));
-		l5.setPreferredSize(new Dimension(80,20));
-		l6.setPreferredSize(new Dimension(80,20));
-		l7.setPreferredSize(new Dimension(80,20));
-		l8.setPreferredSize(new Dimension(80,20));
-		JLabel relleno1=new JLabel("");
-		JLabel relleno2=new JLabel("");
-		JLabel relleno3=new JLabel("");
-		JLabel relleno4=new JLabel("");
-		JLabel relleno5=new JLabel("");
-		relleno1.setPreferredSize(new Dimension(100,20));
-		relleno2.setPreferredSize(new Dimension(150,20));
-		relleno3.setPreferredSize(new Dimension(50,20));
-		relleno4.setPreferredSize(new Dimension(100,20));
-		relleno5.setPreferredSize(new Dimension(20,20));		
-		JTextField tnom=new JTextField();
-		JTextField tdni=new JTextField();
-		JTextField tdir=new JTextField();
-		JTextField tcp=new JTextField();
-		JTextField tloc=new JTextField();
-		JTextField tpro=new JTextField();
-		JTextField ttel=new JTextField();
-		JTextField temail=new JTextField();		
-		tnom.setPreferredSize(new Dimension(200,20));
-		tdni.setPreferredSize(new Dimension(100,20));
-		tdir.setPreferredSize(new Dimension(200,20));
-		tcp.setPreferredSize(new Dimension(60,20));
-		tloc.setPreferredSize(new Dimension(180,20));
-		tpro.setPreferredSize(new Dimension(160,20));
-		ttel.setPreferredSize(new Dimension(80,20));
-		temail.setPreferredSize(new Dimension(80,20));
-		Box c1=Box.createHorizontalBox();
-		Box c2=Box.createHorizontalBox();
-		Box c3=Box.createHorizontalBox();
-		Box c4=Box.createHorizontalBox();
-		Box c5=Box.createHorizontalBox();
-		Box c6=Box.createHorizontalBox();
-		Box c7=Box.createHorizontalBox();
-		Box c8=Box.createHorizontalBox();
-		Box c9=Box.createHorizontalBox();
-		c1.add(l1);
-		c1.add(tnom);
-		c2.add(l2);
-		c2.add(tdni);
-		c2.add(relleno1);
-		c3.add(l3);
-		c3.add(tdir);
-		c4.add(l4);
-		c4.add(tcp);
-		c4.add(relleno2);
-		c5.add(l5);
-		c5.add(tloc);
-		c6.add(l6);
-		c6.add(tpro);
-		c6.add(relleno3);
-		c7.add(l7);
-		c7.add(ttel);
-		c7.add(relleno4);
-		c8.add(l8);
-		c8.add(temail);
-		c8.add(relleno5);
+		formulario.setTitle("Nuevo empleado");
+		JLabel lcodigo=new JLabel("Código del empleado");
+		JLabel lperfil=new JLabel("Perfil del empleado");
+		JLabel lnomina=new JLabel("Nómina del empleado");
+		lcodigo.setPreferredSize(new Dimension(150,20));
+		lperfil.setPreferredSize(new Dimension(150,20));
+		lnomina.setPreferredSize(new Dimension(150,20));
+		JTextField tcodigo=new JTextField();
+		JTextField tperfil=new JTextField();
+		JTextField tnomina=new JTextField();
+		tcodigo.setPreferredSize(new Dimension(100,20));
+		tperfil.setPreferredSize(new Dimension(100,20));
+		tnomina.setPreferredSize(new Dimension(100,20));
+		JPanel pcodigo=new JPanel();
+		JPanel pperfil=new JPanel();
+		JPanel pnomina=new JPanel();
+		pcodigo.add(lcodigo);
+		pcodigo.add(tcodigo);
+		pperfil.add(lperfil);
+		pperfil.add(tperfil);
+		pnomina.add(lnomina);
+		pnomina.add(tnomina);
 		Box caja=Box.createVerticalBox();
-		caja.add(c1);
-		caja.add(c2);
-		caja.add(c3);
-		caja.add(c4);
-		caja.add(c5);
-		caja.add(c6);
-		caja.add(c7);
-		caja.add(c8);
-		JPanel p1=new JPanel();
+		caja.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),"Datos del empleado",TitledBorder.LEFT,TitledBorder.TOP));
+		caja.add(pcodigo);
+		caja.add(pperfil);
+		caja.add(pnomina);
+		JPanel pdat=dibujaDatos(true);
+		JPanel pbotones=new JPanel();
+		JButton bacepta=new JButton ("Aceptar");
+		JButton bcancela=new JButton ("Cancelar");
+		pbotones.add(bacepta);
+		pbotones.add(bcancela);
+		bacepta.addActionListener(new ActionListener()
+				{	public void actionPerformed(ActionEvent e)
+					{
+						formulario.setVisible(false);
+						formulario.getContentPane().removeAll();
+					}
+				});
+		bcancela.addActionListener(new ActionListener()
+				{	public void actionPerformed(ActionEvent e)
+					{
+						formulario.setVisible(false);
+						formulario.getContentPane().removeAll();
+					}
+				});
+		JSplitPane sp1=new JSplitPane(JSplitPane.VERTICAL_SPLIT,caja,pdat);
+		sp1.setDividerSize(4);
+		sp1.setEnabled(false);
+		JSplitPane sp2=new JSplitPane(JSplitPane.VERTICAL_SPLIT,sp1,pbotones);
+		sp2.setDividerSize(4);
+		sp2.setEnabled(false);
+		JPanel pndat=new JPanel();
+		pndat.add(sp2);
+		return pndat;
+	}
+	
+	public void eliminaEmpleado()
+	{
+		JPanel pd=panelBajaEmpleado();
+		formulario.getContentPane().add(pd);
+		formulario.pack();
+		formulario.setVisible(true);
+		formulario.addWindowListener(new WindowAdapter()
+		{	public void windowClosing(WindowEvent e)
+			{
+				formulario.setVisible(false);	
+			}
+		});
+	}
+	
+	public JPanel panelBajaEmpleado()
+	{
+		formulario.setTitle("Despedidiendo empleado...");
+		
+		JLabel lcodigo=new JLabel("Código del empleado");
+		
+		lcodigo.setPreferredSize(new Dimension(150,20));
+		
+		JTextField tcodigo=new JTextField();
+		
+		tcodigo.setPreferredSize(new Dimension(100,20));
+		
+		JPanel pcodigo=new JPanel();
+		
+		pcodigo.add(lcodigo);
+		pcodigo.add(tcodigo);
+		
+		Box caja=Box.createVerticalBox();
+		caja.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),"Datos del empleado",TitledBorder.LEFT,TitledBorder.TOP));
+		caja.add(pcodigo);
+		
+		
 		JPanel p2=new JPanel();
-		p1.add(caja);
 		JButton bacepta=new JButton ("Aceptar");
 		JButton bcancela=new JButton ("Cancelar");
 		p2.add(bacepta);
@@ -331,31 +343,23 @@ public class PanelEmpleados extends JPanel
 		bacepta.addActionListener(new ActionListener()
 				{	public void actionPerformed(ActionEvent e)
 					{
-						nemp.setVisible(false);
+						formulario.setVisible(false);
+						formulario.getContentPane().removeAll();
 					}
 				});
 		bcancela.addActionListener(new ActionListener()
 				{	public void actionPerformed(ActionEvent e)
 					{
-						nemp.setVisible(false);
+						formulario.setVisible(false);
+						formulario.getContentPane().removeAll();
 					}
 				});
-		
-		JSplitPane sp=new JSplitPane(JSplitPane.VERTICAL_SPLIT,p1,p2);
+		JSplitPane sp=new JSplitPane(JSplitPane.VERTICAL_SPLIT,caja,p2);
 		sp.setDividerSize(4);
 		sp.setEnabled(false);
-		
-		
+		JPanel pndat=new JPanel();
 		pndat.add(sp);
-		
-		
-	
 		return pndat;
-	}
-	
-	public void eliminaEmpleado()
-	{
-		
 	}
 	
 	public void modificaEmpleado()
