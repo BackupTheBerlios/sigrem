ad01b8b68bb0c8935ea1c79b9b16b193c8678c98
javi_is <javi_is>
2005-03-14 00:55:50 +0000

    Interfaz acabada. Añadiendo retoques

--- Sigrem/interfaz/InterfazGrafica.java
+++ Sigrem/interfaz/InterfazGrafica.java
@@ -6,12 +6,32 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import javax.swing.filechooser.FileFilter;
+import java.io.*;
+
+class Filtro extends FileFilter
+{
+	public Filtro() 
+	{	super();}
+	
+	public boolean accept(File f)
+	{
+		String nombre=f.getName();
+		return nombre.substring(Math.max(nombre.length()-4,0)).equals(".xml");
+	}
+	public String getDescription()
+	{
+		return "Ficheros de datos (*.xml)";
+	}
+}
 
 public class InterfazGrafica 
 {
 	private JFrame ventana;
 	
 	private JTabbedPane panelVistas;
+	
+	private JFileChooser selec;
 	
 	private JFrame formSigrem;
 	
@@ -34,6 +54,9 @@ public class InterfazGrafica
 		panelVistas.addTab("Gestión Contratos",new PanelContratos());
 		panelVistas.addTab("Gestión Empleados",new PanelEmpleados());
 		panelVistas.addTab("Gestión Económica",new PanelEconomia(0));
+		selec=new JFileChooser(new File(System.getProperty("user.dir")));
+		selec.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		selec.setFileFilter(new Filtro());
 		ventana.getContentPane().add(panelVistas);
 		ventana.setJMenuBar(setMenu());
 		ventana.pack();
@@ -47,6 +70,38 @@ public class InterfazGrafica
 		});	
 	}
 	
+	private void cargar()
+	{
+		try
+		{	if (selec.showOpenDialog(null)==JFileChooser.APPROVE_OPTION)
+			{	File f=selec.getSelectedFile();
+				String nombre=f.getName();
+				BufferedReader entrada=new BufferedReader(new FileReader(nombre));
+				String text=new String();
+				while ((text=entrada.readLine())!=null)
+				{		}
+			}
+		}
+		catch (Exception ex)
+		{	JOptionPane.showMessageDialog(null,"Error al cargar","Sigrem",-1);}
+	}
+	
+	private void guardar()
+	{
+		try
+		{	if (selec.showSaveDialog(null)==JFileChooser.APPROVE_OPTION)
+			{	String nombre=selec.getSelectedFile().getName();
+				if (nombre.substring(Math.max(nombre.length()-4,0)).equals(".xml"))
+				{	nombre=nombre.substring(0,nombre.length()-4);}
+				PrintWriter ficheroSal=new PrintWriter(new BufferedWriter(new FileWriter(nombre+".xml")));
+				ficheroSal.println("Esto es una prueba");
+				ficheroSal.close();
+			}
+		}
+		catch (Exception ex)
+		{	JOptionPane.showMessageDialog(null,"Error al guardar","Sigrem",-1);}		
+	}
+	
 	public void activa()
 	{
 		ventana.setVisible(true);
@@ -58,16 +113,14 @@ public class InterfazGrafica
 		JMenu m1=new JMenu("Archivo");
 		JMenu m3=new JMenu("Herramientas");
 		JMenu m4=new JMenu("Acerca de");
-		JMenuItem mac=new JMenuItem("Cargar datos");
-		JMenuItem mgc=new JMenuItem("Guardar datos");
-		JMenuItem mgg=new JMenuItem("Guardar datos como");
+		JMenuItem cargar=new JMenuItem("Cargar datos");
+		JMenuItem guardar=new JMenuItem("Guardar datos");
 		JMenuItem salir=new JMenuItem("Salir");
 		JMenuItem ayuda=new JMenuItem("Ayuda");
 		JMenuItem acercade=new JMenuItem("Acerca de");
-		m1.add(mac);
-		m1.add(mgc);
-		m1.add(mgg);
-		m1.add(new JSeparator());
+		m1.add(cargar);
+		m1.add(guardar);
+		m1.addSeparator();
 		m1.add(salir);
 		JMenu consul=new JMenu("Consultar");
 		JMenu cont=new JMenu("Contrato");
@@ -114,7 +167,7 @@ public class InterfazGrafica
 		m3.add(consul);
 		m3.add(adm);
 		m4.add(ayuda);
-		m4.add(new JSeparator());
+		m4.addSeparator();
 		m4.add(acercade);
 		menu.add(m1);
 		menu.add(m3);
@@ -274,6 +327,18 @@ public class InterfazGrafica
 				panelVistas.remove(2);
 				panelVistas.addTab("Gestión Económica",new PanelEconomia(3));
 				panelVistas.setSelectedIndex(2);
+			}
+		});
+		cargar.addActionListener(new ActionListener()
+		{	public void actionPerformed(ActionEvent e)
+			{
+				cargar();		
+			}
+		});
+		guardar.addActionListener(new ActionListener()
+		{	public void actionPerformed(ActionEvent e)
+			{
+				guardar();
 			}
 		});
 		return menu;	
