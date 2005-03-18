b8029a5afc64e8f478cd60fbf6c5c5e9f1f78ea0
javi_is <javi_is>
2005-03-18 18:26:27 +0000

    *** empty log message ***

--- Sigrem/med/EstructuraDatosImp.java
+++ Sigrem/med/EstructuraDatosImp.java
@@ -1,21 +1,7 @@
-/*
- * Created on 14-mar-2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package med;
 
 
 import java.util.*;
-
-import main.Sigrem;
-/**
- * @author frank
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 
 public class EstructuraDatosImp implements EstructuraDatos{
 	  Indice[] arrayIndices;
