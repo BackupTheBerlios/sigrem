7814a52dbc88781c7ea155646532a47c604f18c9
miguelr80 <miguelr80>
2005-03-24 04:03:32 +0000

    Relleno

--- Sigrem/mre/GRecursos.java
+++ Sigrem/mre/GRecursos.java
@@ -1,6 +1,15 @@
 package mre;
 
+import java.util.LinkedList;
+
 public interface GRecursos 
 {
-
+	//Requisito 4.1
+	public String añadirRecurso(LinkedList datos);
+	//Requisito 4.2
+	public void eliminarRecurso(String codigo);
+	//Requisito 4.3
+	public void modificarRecurso(String codigo);
+	//Requisito 4.4
+	public void consultarRecursoCodigo(String codigo);
 }
