677064f04079ae6463b528b1e61548e88e05483d
miguelr80 <miguelr80>
2005-03-29 16:41:28 +0000

    *** empty log message ***

--- Sigrem/mec/GEconomia.java
+++ Sigrem/mec/GEconomia.java
@@ -2,9 +2,12 @@ package mec;
 
 public interface GEconomia 
 {
+	//Requisito 5.3
 	public void balance();
 		
+	//Requisito 5.2
 	public void gastos();
 	
+	//Requisito 5.1
 	public void facturacion();	
 }
