3e2cec281e498127eefb63b3f0a8fc9f9608bd34
miguelr80 <miguelr80>
2005-03-30 20:59:06 +0000

    Cambiando cosillas

--- Sigrem/ClasePrincipal.java
+++ Sigrem/ClasePrincipal.java
@@ -16,9 +16,17 @@ public class ClasePrincipal
 	
 	private static String codrecurso;
 	
-	private static String codempleado;	
+	private static String codempleado;
 	
-		private static void leerFicheroConfig()
+	private static String mes;
+	
+	private static String facturacion;
+	
+	private static String gastos;
+	
+	private static String balance;
+	
+	private static void leerFicheroConfig()
 	{
 		try
 		{	BufferedReader entrada=new BufferedReader(new FileReader("configsig.ini"));
@@ -36,7 +44,15 @@ public class ClasePrincipal
 			codrecurso=cod.substring(pos+1);
 			cod=entrada.readLine();
 			pos=cod.indexOf('=');
-			codempleado=cod.substring(pos+1);		
+			codempleado=cod.substring(pos+1);
+			pos=cod.indexOf('=');
+			mes=cod.substring(pos+1);
+			pos=cod.indexOf('=');
+			facturacion=cod.substring(pos+1);
+			pos=cod.indexOf('=');
+			gastos=cod.substring(pos+1);
+			pos=cod.indexOf('=');
+			balance=cod.substring(pos+1);			
 		}
 		catch (Exception ex)
 		{	JOptionPane.showMessageDialog(null,"Error al cargar el fichero de configuración","Sigrem",-1);
@@ -48,12 +64,16 @@ public class ClasePrincipal
 	{
 		System.out.println("El programa Sigrem ha comenzado");
 		leerFicheroConfig();
-		String[] codigos=new String[5];
+		String[] codigos=new String[9];
 		codigos[0]=codcontrato;
 		codigos[1]=codcliente;
 		codigos[2]=codmulta;
 		codigos[3]=codrecurso;
 		codigos[4]=codempleado;
+		codigos[5]=mes;
+		codigos[6]=facturacion;
+		codigos[7]=gastos;
+		codigos[8]=balance;
 		controlador=new Sigrem(codigos);
 		controlador.activa();
 	}
